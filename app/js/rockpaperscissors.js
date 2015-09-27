cc4e352b6722fc53ad0beacc7b69a7a38a579757
Sungwon Ryu <swryu0906@gmail.com>
2015-09-27 14:53:17 -0400

    Modifies getPlayMove() and getComputerMove() functions.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,14 +25,16 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    move = move || 'getInput()';
+    return move;
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    move = move || 'randomPlay()';
+    return move;
 }
 
 function getWinner(playerMove,computerMove) {
@@ -52,4 +54,3 @@ function playToFive() {
     /* YOUR CODE HERE */
     return [playerWins, computerWins];
 }
-
